2212e3850c16d8be872af4c9fd63fc547015f255
YamaKasa3 <yt.const@gmail.com>
2020-12-22 00:41:08 +0900

    修正

--- core/ClassLoader.php
+++ core/ClassLoader.php
@@ -1,1 +1,31 @@
 <?php
+
+class ClassLoader
+{
+    protected $dirs;
+
+    public function regster()
+    {
+        /*
+        SPL The Standard PHP Library
+        ファイルを読み込んだときに、そのファイルがないときのエラーに対処する
+        */
+        spl_autoload_register(array($this, 'loadClass'));
+    }
+
+    public function registerDir($dir)
+    {
+        $this->dirs[] = $dir; // array_push
+    }
+
+    public function loadClass($class)
+    {
+        foreach ($this->dirs as $dir) {
+            $file = $dir . '/' . $class . '.php';
+            if (is_readable($file)) {
+                require $file;
+                return;
+            }
+        }
+    }
+}
